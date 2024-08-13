48e99510b944521e4c862a637e87a38a5d7fa984
Bruno Marques <brunomarques.1@hotmail.com>
2024-08-13 13:00:20 -0300

    +

--- AT1_PB/ec2.tf
+++ AT1_PB/ec2.tf
@@ -1,12 +1,16 @@
 resource "aws_instance" "web_server" {
-  ami             = "ami-034547d56642a9dac"
+  ami             = "ami-0dfdffe179243455b"
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.public_subnet.id
   security_groups = [aws_security_group.ec2_sg.id]
   key_name = "key_pair"
 
+  user_data = templatefile("${path.module}/script.sh.tpl", {
+    efs_ip = aws_efs_mount_target.mount_target.ip_address
+  })
+
 
   tags = {
-    Name = "Web-Server_Apache"
+    Name = "Web-Server-Apache"
   }
 }
